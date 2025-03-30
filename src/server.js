e84070fef66c0b0893603e683538b7c8b0adea8b
Roman Soltan <romabandit88@gmail.com>
2025-03-30 13:04:07 +0300

    step 2

---
+++ src/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import { getEnvVar } from './utils/getEnvVar.js';
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use((req, res) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  const port = Number(getEnvVar('PORT', 3000));
+  app.listen(port, () => console.log(`Server is running on port ${port}`));
+};
