1a815b39d1c8831cb19b5d30071af74eeaaf1e1d
Roman Soltan <romabandit88@gmail.com>
2025-03-30 15:26:25 +0300

    step 4

---
+++ src/db/models/Contact.js
@@ -0,0 +1,34 @@
+import { Schema, model } from 'mongoose';
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    isFavorite: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const contactsCollection = model('contacts', contactSchema);
