f50e6f2973cb128a6834be76642ca07b24063247
Roman Soltan <romabandit88@gmail.com>
2025-04-13 12:55:21 +0300

    step 2: validation id,validateBody, addContactSchema, updateContactSchema

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,17 +8,40 @@ import {
   patchContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  addContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
 
 const contactsRouter = Router();
 
 contactsRouter.get('/', ctrlWrapper(getAllContactsController));
 
-contactsRouter.get('/:contactId', ctrlWrapper(getAllContactByIdController));
+contactsRouter.get(
+  '/:contactId',
+  isValidId,
+  ctrlWrapper(getAllContactByIdController),
+);
 
-contactsRouter.post('/', ctrlWrapper(addContactController));
+contactsRouter.post(
+  '/',
+  validateBody(addContactSchema),
+  ctrlWrapper(addContactController),
+);
 
-contactsRouter.patch('/:contactId', ctrlWrapper(patchContactController));
+contactsRouter.patch(
+  '/:contactId',
+  validateBody(updateContactSchema),
+  isValidId,
+  ctrlWrapper(patchContactController),
+);
 
-contactsRouter.delete('/:contactId', ctrlWrapper(deleteContactController));
+contactsRouter.delete(
+  '/:contactId',
+  isValidId,
+  ctrlWrapper(deleteContactController),
+);
 
 export default contactsRouter;
---
+++ src/validation/contacts.js
@@ -0,0 +1,26 @@
+import Joi from 'joi';
+import { contactTypeList } from '../constants/contacts.js';
+
+export const addContactSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required().messages({
+    'any.required': 'Name is required',
+    'string.empty': 'Name cannot be empty.',
+    'string.min': 'User name should be at least 3 characters long.',
+    'string.max': 'User name should not exceed 30 characters.',
+  }),
+  phoneNumber: Joi.string().required().messages({
+    'any.required': 'Phone number is required',
+    'string.empty': 'Phone number cannot be empty.',
+  }),
+  email: Joi.string().email(),
+  isFavorite: Joi.boolean(),
+  contactType: Joi.string().valid(...contactTypeList),
+});
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string(),
+  phoneNumber: Joi.string(),
+  email: Joi.string().email(),
+  isFavorite: Joi.boolean(),
+  contactType: Joi.string().valid(...contactTypeList),
+});
