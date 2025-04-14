7e3f1db93e2520528081e9d577c80dfc97489e54
Roman Soltan <romabandit88@gmail.com>
2025-04-14 07:13:27 +0300

    filter contactType

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -7,11 +7,17 @@ export const getAllContacts = async ({
   perPage = 10,
   sortOrder = SORT_ORDER.ASC,
   sortBy = '_id',
+  filter = {},
 }) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
   const contactsQuery = ContactsCollection.find();
+
+  if (filter.type) {
+    contactsQuery.where('type').equals(filter.type);
+  }
+
   const totalItems = await ContactsCollection.find()
     .merge(contactsQuery)
     .countDocuments();
---
+++ src/utils/parseFilterParams.js
@@ -0,0 +1,16 @@
+const parseContactType = (type) => {
+  const isString = typeof type === 'string';
+  if (!isString) return;
+
+  const isContactType = (type) => ['work', 'home', 'personal'].includes(type);
+
+  if (isContactType(type)) return type;
+};
+
+export const parseFilterParams = (query) => {
+  const { type } = query;
+
+  const parsedContactType = parseContactType(type);
+
+  return { type: parsedContactType };
+};
