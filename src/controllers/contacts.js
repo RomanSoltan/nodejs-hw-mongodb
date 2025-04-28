78e38ade96c9ae54cfe54c68438af8b312006ce0
Roman Soltan <romabandit88@gmail.com>
2025-04-28 18:48:02 +0300

    step 6: fix route /contacts, add upload photo

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -55,7 +55,19 @@ export const getAllContactByIdController = async (req, res) => {
 
 export const addContactController = async (req, res) => {
   const { _id: userId } = req.user;
-  const data = await addContact({ ...req.body, userId });
+  const photo = req.file;
+
+  let photoUrl;
+
+  if (photo) {
+    if (getEnvVar('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
+  }
+
+  const data = await addContact({ ...req.body, userId, photo: photoUrl });
 
   res.status(201).json({
     status: 201,
